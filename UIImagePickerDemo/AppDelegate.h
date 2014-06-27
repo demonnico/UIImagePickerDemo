26ed1608808e937b8dfa81ee4a16b506a322da4c
Nicholas Tau <fengping.tao@upai.com>
2014-06-27 11:38:35 +0800

    Initial Commit

---
+++ UIImagePickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImagePickerDemo
+//
+//  Created by Nicholas Tau on 6/27/14.
+//  Copyright (c) 2014 Nicholas Tau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
